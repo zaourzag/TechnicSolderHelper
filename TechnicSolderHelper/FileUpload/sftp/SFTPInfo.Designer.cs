807b8f971b1ec021f0d1485ae9b8edf681994758
GabrielTK <enricorocha1@hotmail.com>
2017-09-06 17:44:51 -0300

    Removed the label

--- TechnicSolderHelper/FileUpload/sftp/SFTPInfo.Designer.cs
+++ TechnicSolderHelper/FileUpload/sftp/SFTPInfo.Designer.cs
@@ -41,7 +41,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.port)).BeginInit();
             this.SuspendLayout();
             // 
@@ -164,21 +163,11 @@
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(138, 270);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(205, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "--> Make sure there is no file named \"test\"";
-            // 
             // SFTPInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(361, 376);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -216,6 +205,5 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label6;
     }
 }
